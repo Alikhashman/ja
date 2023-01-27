c6afd7c2b5cfef30bdc5853b895dbeb6376909b7
Alikhashman <alikhashman670@gmail.com>
2023-01-27 17:42:33 +0300

    aa

---
+++ script.js
@@ -0,0 +1,35 @@
+// document.getElementById("date").innerHTML = Date();
+// document.getElementById("date").innerHTML = "ali khashman";
+// document.getElementById("date").innerHTML = 23 + 5;
+// document.getElementById("date").innerHTML = "<span style='color:red';>ali<span/>"
+// 2
+document.write(Date());
+document.write("<br>");
+document.write("alikhashman");
+document.write("<br>");
+document.write(35 + 9);
+document.write("<br>")
+document.write("<span style='color:red';>ali<span/>")
+
+
+//3
+// alert("hello");
+
+// 4
+// للمساعدة
+console.log("hello from console")
+document.write(`
+
+ <div class="card">
+        <div class="img">
+            <img src="2.jpg" alt="">
+        </div>
+        <div class="title">
+            <h1>product</h1>
+        </div>
+    </div>
+
+
+
+
+`);
